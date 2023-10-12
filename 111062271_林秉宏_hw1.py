96cc0980ec760d1ffe2024fd6606975cc01f473c
linskybing <linskybing@gmail.com>
2023-10-12 15:58:32 +0800

    update

--- 111062271_林秉宏_hw1.py
+++ 111062271_林秉宏_hw1.py
@@ -11,18 +11,16 @@ class System:
     def __init__(self):
         #member variable
         self.user_account = ''
-        self.user_password = ''
-        self.cookie = False        
-        self.DataBaseCheck()
+        self.cookie = False
         self.CookieCheck()
-        self.database_ob = DataBase(self.user_account)
+        self.database_ob = DataBase()
 
         # method        
         self.Loading()        
-
+    
     def Loading(self):
-
-        print('Welcome to the pymoeny\n')
+        
+        print('\nWelcome to the pymoeny\n')
 
         # not login
         if (not(self.cookie)):
@@ -44,14 +42,14 @@ class System:
         else:
             mode = 0
             
-            print(f'Hello {self.user_account:5s}!\n Which features do you want ?\n (1 : Add some financial records) or (2 : Show the history records) or (3 : Logout) or (-1: Exist)\n')
+            print(f'Hello {self.user_account}!\n\nWhich features do you want ?\n\n (1 : Add some financial records) or (2 : Show the history records) or (3 : Logout) or (Other keyword: Exist)\n')
             mode = int(input('Please choose the number of features that you want: '))
 
             if (mode == 1):
-                self.database_ob.AddRecord(self.user_account)                    
-
+                self.database_ob.AddRecord(self.user_account) 
+                
             elif (mode == 2):
-                self.database_ob.ShowHistory(self.user_account)                    
+                self.database_ob.ShowHistory(self.user_account)                   
 
             elif (mode == 3):
                 self.Logout()                    
@@ -60,24 +58,6 @@ class System:
                 pass
 
 
-
-    # check database dir exist or not
-    # if don't exist then create a new one
-    def DataBaseCheck(self):
-
-        if (not(os.path.exists(user_infomation_path))):
-            fh = open(user_infomation_path, 'w')
-            fh.close()
-        else :
-
-            #load userdata
-            fh = open(user_infomation_path, 'r')  
-            user_data = fh.readline().split()
-            fh.close()
-
-            if(user_data):
-                self.user_account = user_data[0]
-                self.user_password = user_data[1]
 
     # this is a user cookie check
     # that use to determine user has already login
@@ -90,8 +70,10 @@ class System:
         
         fh = open(cookie_path, 'r')
         data = fh.readline()
-        if (data == '1'):
-            self.cookie = True 
+        if (data and data != '0' ):
+            self.cookie = True
+            self.user_account = data
+            
         else:
             self.cookie = False 
         
@@ -103,14 +85,15 @@ class System:
         password = input('{:6s}'.format('Password: '))
         
         # CheckUserDataExist
-        if (self.user_account and self.user_password):
+        if (self.database_ob.AccountExist(account)):
             
-            if (account == self.user_account and password == self.user_password):
-                self.cookie = True
-                self.WriteCookie(1)
-
+            if (self.database_ob.CheckLogin(account, password)):                
+                self.user_account = account
+                self.WriteCookie(account)
+                self.Loading()
+                
             else:
-                print("Account or Password doesn't match.\n")
+                print("Account or Password doesn't match.")
         else :
             print('Account does not exist !!')                            
     
@@ -120,33 +103,24 @@ class System:
         account = input('{:6s}'.format('Account: '))
         password = input('{:6s}'.format('Password: '))
 
-        if (account == self.user_account):
+        if (self.database_ob.AccountExist(account)):
             print('Account has already exist.')
-        else :
-            #write data into database
-            fh = open(user_infomation_path, 'r')
-            if(len(fh.readlines()) > 0):
-                fh = open(user_infomation_path, 'a')
-                fh.write(f'\n{account}\t{password}')
-                fh.close()
-            else:
-                fh = open(user_infomation_path, 'w')
-                fh.write(f'{account}\t{password}')
-                fh.close()
-
+        else :            
+            self.database_ob.RegisterAccount(account, password)           
+            
             #success register 
-            self.WriteCookie(1)
-            self.DataBaseCheck()
+            self.user_account = account
+            self.WriteCookie(account)
             self.Loading()
 
-    def WriteCookie(self, flag):
+    def WriteCookie(self, account):
 
         # to set user cookie is active or inactive
         # if cookie is active, then it is mean that user has already been login
         # in other hand, user has not been login yet.        
-        if (flag):
+        if (account):
             fh = open(cookie_path, 'w')
-            fh.write('1')
+            fh.write(account)
             fh.close()
             self.cookie = True
         else:
@@ -156,27 +130,42 @@ class System:
             self.cookie = False
 
     def Logout(self):
-        self.WriteCookie(0)
+        self.WriteCookie('0')
 
 class DataBase:
 
-    def __init__(self, account):
+    def __init__(self):
 
         #member variable
         self.balance_data = {}
         self.financial_records = {}
-        
+        self.member_account = {}
         #member method
-        self.ReadUserRecord(account)
+        self.ReadUserRecord()
         self.ReadBalanceData()
-
+        self.ReadUserAccount()
 
     #############################################
     # About Read database                       #
     #############################################
+    
+    def ReadUserAccount(self):
+        # check user account file exist
+        if (not(os.path.exists(user_infomation_path))):
+            fh = open(user_infomation_path, 'w')
+            fh.close()
+        
+        # read user account data from file
+        fh = open(user_infomation_path, 'r')
+        file = fh.readlines()
+        fh.close()
+        
+        for data in file:
+            account, password = data.split()
+            self.member_account[account] = password
 
     #read the record of the user    
-    def ReadUserRecord(self, account):
+    def ReadUserRecord(self):
         #check database whether user financial records exist or not
         if (not(os.path.exists(user_data_record))):
             fh = open(user_data_record, 'w')
@@ -194,11 +183,16 @@ class DataBase:
         #        'change': [],
         #   },
         # }
-        self.financial_records[account] = {}
-        self.financial_records[account]['balance'] = []
-        self.financial_records[account]['change'] = []
+        
+        last = ''
         for i in range(0, len(data)):
             account, description, change = data[i].split()
+            
+            if (account != last):                
+                self.financial_records[account] = {}
+                self.financial_records[account]['balance'] = []
+                self.financial_records[account]['change'] = []
+                last = account
             self.financial_records[account]['balance'].append(description)
             self.financial_records[account]['change'].append(int(change))
 
@@ -217,9 +211,24 @@ class DataBase:
         fh.close()    
 
     #############################################
-    # About Modify data                         #
+    # About Modify File                         #
     #############################################
-
+    
+    def RegisterAccount(self, account, password):
+        
+        #write data into database
+        fh = open(user_infomation_path, 'r')
+        if(len(fh.readlines()) > 0):
+            fh = open(user_infomation_path, 'a')
+            fh.write(f'\n{account}\t{password}')
+            fh.close()
+        else:
+            fh = open(user_infomation_path, 'w')
+            fh.write(f'{account}\t{password}')
+            fh.close()
+        
+        self.member_account[account] = password
+        
     #Add some financial records
     def AddRecord(self, account):
 
@@ -227,6 +236,7 @@ class DataBase:
         if (account not in self.balance_data):
             money = int(input('How much money do you have? '))
             self.balance_data[account] = money
+            self.financial_records[account] = {}
             self.financial_records[account]['balance'] = []
             self.financial_records[account]['change'] = []
 
@@ -236,22 +246,22 @@ class DataBase:
         # maybe there are many reocrd that the user want to reocrd
         # then we use data_list that record seperated by ','
         # deal with data by sequence
-
+        
         for data in data_list:
-
+            data = data.strip()
             description, change = data.split(' ')
             change = int(change)            
             self.financial_records[account]['balance'].append(description)
             self.financial_records[account]['change'].append(int(change))  
 
             self.balance_data[account] += change
-            print(f'Now you have {self.balance_data[account]} dollars.\n')
-
-        self.WriteRecord(account)
+            
+        print(f'Now you have {self.balance_data[account]} dollars.')
+        self.WriteRecord()
         self.ChangeBalance()
 
     # update record in database
-    def WriteRecord(self, account):
+    def WriteRecord(self):
 
         fh = open(user_data_record, 'w')        
         for account in self.financial_records:
@@ -269,25 +279,46 @@ class DataBase:
             fh.write(f'{account}\t{self.balance_data[account]}\n')
         
         fh.close()       
+        
+    #############################################
+    # Useful Check Function                     #
+    #############################################
+    
+    # Check user account whether has already existed
+    def AccountExist(self, account):
+        
+        if (account in self.member_account):
+            return True
+        
+        return False
+    
+    # check user account information whether is correct
+    def CheckLogin(self, account, password):
+        if (password == self.member_account[account]):
+            return True
+        
+        return False
     
     #############################################
     # About Showing data                        #
     #############################################
 
-    def ShowHistory(self, account):
-        if (account in self.balance_data):
-            print(f'Now you have {self.balance_data[account]} dollars.\n')
+    def ShowHistory(self, u_account):
+        if (u_account in self.balance_data):
+            print(f'Now you have {self.balance_data[u_account]} dollars.\n')
             print(f'------your record history------\n')
 
             for account in self.financial_records:
-
-                records = self.financial_records[account]            
-                for i in range(0, len(records['balance'])):            
-                    print(f"{self.financial_records[account]['balance'][i]:10s}{self.financial_records[account]['change'][i]:10d}")  
+                
+                if (u_account == account):
+                    records = self.financial_records[account]            
+                    for i in range(0, len(records['balance'])):            
+                        print(f"{self.financial_records[account]['balance'][i]:10s}{self.financial_records[account]['change'][i]:10d}")  
 
             print(f'-------------------------------\n')
 
-        else:
+        else:            
             print(f"you don't have any record. please add new record first")
+            
 if __name__ == '__main__':   
     sys = System()
